df72b9cd164d6e2f2cd5efc9b849ae9c9244f186
kittipong-pc <kittipong.ma.59@ubu.ac.th>
2019-07-20 17:59:26 +0700

    new start template

--- web/src/views/Start.vue
+++ web/src/views/Start.vue
@@ -1,17 +1,37 @@
-<template>
-  <div>
-    <StartNavbar></StartNavbar>
+<template lang="html">
+  <div id="app">
+    <div class="container-scroller">
+      <StartNavbar/>
+      <div class="container-fluid page-body-wrapper px-0 ">
+        <StartSidebar/>
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <router-view></router-view>
+          </div>
+          <!-- content wrapper ends -->
+          
+        </div>
+        <!-- main panel ends -->
+      </div>
+      <StartFooter/>
+      <!-- page-body-wrapper ends -->
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import StartNavbar from '@/components/StartNavbar.vue'
+import StartSidebar from '@/components/StartSidebar.vue'
+import StartFooter from '@/components/StartFooter.vue'
 
 export default {
   name: 'start',
   components: {
-    StartNavbar
+    StartNavbar,
+    StartSidebar,
+    StartFooter
   }
 }
 </script>
+
